884aad3715384cf3a7656e6ef25bf201e0865a1e
Aditya <58824912+TriStaRvOiD@users.noreply.github.com>
2023-08-08 03:17:15 +0530

    Increased version to 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.tristarvoid.qrscanner"
         minSdk = 21
         targetSdk = 33
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "2.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
@@ -30,6 +30,7 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+            ndk.debugSymbolLevel = "SYMBOL_TABLE"
         }
     }
     buildFeatures {
