836dd31d2de6e0d40183e61d18160420bc366a9e
Aditya <58824912+TriStaRvOiD@users.noreply.github.com>
2023-12-18 11:21:47 +0530

    Bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.tristarvoid.qrscanner"
         minSdk = 24
         targetSdk = 34
-        versionCode = 5
-        versionName = "2.0.3"
+        versionCode = 6
+        versionName = "2.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
